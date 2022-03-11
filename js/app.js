f9fa3e6cc41de1e7987c7b2a3bafdbfa3ec38830
jicu mollick <jikumollick82@gmail.com>
2022-03-11 13:01:55 +0600

    report tab fixed

--- js/app.js
+++ js/app.js
@@ -47,7 +47,8 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
-
+        
+        document.getElementById( "reported") .innerHTML = ''; // cleaning the body
         displayReportedPosts();
     }
 };
@@ -157,7 +158,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
