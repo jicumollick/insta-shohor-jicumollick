8b7bfe723ede1f238d93d904ece44a45d329a666
jicu mollick <jikumollick82@gmail.com>
2022-03-11 12:38:49 +0600

    heart button and user profile pic fixed

--- js/app.js
+++ js/app.js
@@ -16,7 +16,9 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+ 
+    // likedPostsId.plus(id); 
+    likedPostsId.pus(id); 
     showPosts(posts);
 };
 
@@ -51,6 +53,8 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+ 
+  
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -62,7 +66,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
